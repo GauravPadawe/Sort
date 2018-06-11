e6e55d4c8eba7bf2931a3fb4dbd57760ff24c4ed
Gaurav Padawe <grvpdw92@gmail.com>
2018-06-11 23:37:31 +0530

    Initial commit

---
+++ Sort.py
@@ -0,0 +1,15 @@
+﻿a = "without,hello,bag,world"              
+
+b = a.split(',')                 # Conversion of string to list
+
+print (b)                        # printing out b for reference
+
+b.sort()                         # Sorting out b in sequence (Note: sorting changes value forever)
+
+print (b)                        # for reference
+
+d = (','.join(b))                # Joining the list by comma to convert it into string
+
+print (d)                        # print d
+
+# CODED BY - GAURAV PADAWE
